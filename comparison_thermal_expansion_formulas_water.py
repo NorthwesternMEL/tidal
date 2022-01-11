03d152be79feb789ce9ba5fcfac58a0f03bcddd3
Jibril Coulibaly <jibril.coulibaly@gmail.com>
2022-01-10 22:37:12 -0600

    rename volumetric thermal coefficient variables bs and bw instead of as and aw

--- comparison_thermal_expansion_formulas_water.py
+++ comparison_thermal_expansion_formulas_water.py
@@ -53,22 +53,22 @@ temp = np.arange(tlo, thi + 0.5*tincr, tincr, dtype=float)
 # expansion is calculated with 2nd order central differences at tlo and thi
 tempad = np.arange(tlo-tincr, thi + 0.5*tincr + tincr, tincr, dtype=float)
 
-aw_Baldi88 = thexp.coef_w_Baldi88(patm,temp) # Baldi et al., 1988
-aw_Cekerevac05 = thexp.coef_w_Cekerevac05(patm,temp) # Cekerevac et al., 2005
-aw_Smith54 = thexp.coef_w_Smith54(tcw, temp) # Smith et al., 2005
-aw_Chapman74 = thexp.coef_w_Chapman74(temp) # Chapman, 1974
-aw_IAPWS95 = thexp.coef_w_IAPWS95_tab("dat_IAPWS95_1atm_10-90-0.5degC",
+bw_Baldi88 = thexp.coef_w_Baldi88(patm,temp) # Baldi et al., 1988
+bw_Cekerevac05 = thexp.coef_w_Cekerevac05(patm,temp) # Cekerevac et al., 2005
+bw_Smith54 = thexp.coef_w_Smith54(tcw, temp) # Smith et al., 2005
+bw_Chapman74 = thexp.coef_w_Chapman74(temp) # Chapman, 1974
+bw_IAPWS95 = thexp.coef_w_IAPWS95_tab("dat_IAPWS95_1atm_10-90-0.5degC",
                                       tempad)[0][1:-1] # IAPWS-95
-aw_CRC40ed = thexp.coef_w_CRC40ed(temp) # CRC Handbook 40th ed, 1958-1959
+bw_CRC40ed = thexp.coef_w_CRC40ed(temp) # CRC Handbook 40th ed, 1958-1959
 
 # Plot results and export to comma-separated tables
 plt.figure(1)
-plt.plot(temp, aw_Baldi88*1e4, label="Baldi et al., 1988 (p=1 atm)")
-plt.plot(temp, aw_Cekerevac05*1e4, label="Cekerevac et al., 2005 (p=1 atm)")
-plt.plot(temp, aw_Smith54*1e4, label="Smith et al., 1954")
-plt.plot(temp, aw_Chapman74*1e4, label="Chapman, 1974")
-plt.plot(temp, aw_IAPWS95*1e4, label="IAPWS-95 (p=1 atm)")
-plt.plot(temp, aw_CRC40ed*1e4, label="CRC Handbook, 40th ed.")
+plt.plot(temp, bw_Baldi88*1e4, label="Baldi et al., 1988 (p=1 atm)")
+plt.plot(temp, bw_Cekerevac05*1e4, label="Cekerevac et al., 2005 (p=1 atm)")
+plt.plot(temp, bw_Smith54*1e4, label="Smith et al., 1954")
+plt.plot(temp, bw_Chapman74*1e4, label="Chapman, 1974")
+plt.plot(temp, bw_IAPWS95*1e4, label="IAPWS-95 (p=1 atm)")
+plt.plot(temp, bw_CRC40ed*1e4, label="CRC Handbook, 40th ed.")
 plt.xlim(20, 80) # Limit plot to [ 20 ; 80] degC
 plt.title("Figure 2a of Coulibaly and Rotta Loria 2022")
 plt.xlabel('Temperature [degC]')
@@ -76,15 +76,15 @@ plt.ylabel('Volumetric thermal expansion of water [$10^{-4}$ /degC]')
 plt.legend()
 
 np.savetxt("tab_comparison_thermal_expansion_formulas_water.csv",
-           np.concatenate((temp[:,np.newaxis], aw_Baldi88[:,np.newaxis],
-                           aw_Cekerevac05[:,np.newaxis],
-                           aw_Smith54[:,np.newaxis], aw_Chapman74[:,np.newaxis],
-                           aw_IAPWS95[:,np.newaxis], aw_CRC40ed[:,np.newaxis]),
+           np.concatenate((temp[:,np.newaxis], bw_Baldi88[:,np.newaxis],
+                           bw_Cekerevac05[:,np.newaxis],
+                           bw_Smith54[:,np.newaxis], bw_Chapman74[:,np.newaxis],
+                           bw_IAPWS95[:,np.newaxis], bw_CRC40ed[:,np.newaxis]),
                           axis=1),
-           header=("temperature_degC,aw_Baldi88_1atm_1perdegC,"+
-                   "aw_Cekerevac05_1atm_1perdegC,aw_Smith54_1perdegC,"+
-                   "aw_Chapman74_1perdegC,aw_IAPWS95_1atm_1perdegC,"+
-                   "aw_CRC40ed_1perdegC"),
+           header=("temperature_degC,bw_Baldi88_1atm_1perdegC,"+
+                   "bw_Cekerevac05_1atm_1perdegC,bw_Smith54_1perdegC,"+
+                   "bw_Chapman74_1perdegC,bw_IAPWS95_1atm_1perdegC,"+
+                   "bw_CRC40ed_1perdegC"),
            delimiter=',')
 
 # ------------------------------------------------------------------------------
@@ -94,13 +94,13 @@ np.savetxt("tab_comparison_thermal_expansion_formulas_water.csv",
 # ------------------------------------------------------------------------------
 
 # Baldi et al., 1988
-aw_Baldi88_50kPa = thexp.coef_w_Baldi88(50e3,temp)
-aw_Baldi88_200kPa = thexp.coef_w_Baldi88(200e3,temp)
-aw_Baldi88_400kPa = thexp.coef_w_Baldi88(400e3,temp)
-aw_Baldi88_1000kPa = thexp.coef_w_Baldi88(1000e3,temp)
+bw_Baldi88_50kPa = thexp.coef_w_Baldi88(50e3,temp)
+bw_Baldi88_200kPa = thexp.coef_w_Baldi88(200e3,temp)
+bw_Baldi88_400kPa = thexp.coef_w_Baldi88(400e3,temp)
+bw_Baldi88_1000kPa = thexp.coef_w_Baldi88(1000e3,temp)
 
 # IAPWS-95: tabulated values
-aw_IAPWS95_p = [
+bw_IAPWS95_p = [
   thexp.coef_w_IAPWS95_tab("dat_IAPWS95_50kPa_10-90-0.5degC", tempad)[0][1:-1],
   thexp.coef_w_IAPWS95_tab("dat_IAPWS95_200kPa_10-90-0.5degC", tempad)[0][1:-1],
   thexp.coef_w_IAPWS95_tab("dat_IAPWS95_400kPa_10-90-0.5degC", tempad)[0][1:-1],
@@ -108,43 +108,43 @@ aw_IAPWS95_p = [
   
 # IAPWS-95: 3rd degree polynomial fitting of the IAPWS-95 values. Temperature
 # range T=[20 ; 80] degC and pressure range p=[50 ; 1000] kPa.
-# The coefficients obtained in the variable `aw_IAPWS95_coef` below are used to
+# The coefficients obtained in the variable `bw_IAPWS95_coef` below are used to
 # inform the preset 'd3_t20-80_p50-1000' of the fitting function named
 # 'coef_w_IAPWS95_fit' in the 'thexp' module
-aw_IAPWS95_fit, aw_IAPWS95_coef = thexp.coef_w_IAPWS95_fit('compute', temp,
-                                                           aw_IAPWS95_p, 3)
+bw_IAPWS95_fit, bw_IAPWS95_coef = thexp.coef_w_IAPWS95_fit('compute', temp,
+                                                           bw_IAPWS95_p, 3)
 
 # Plot results and export to comma-separated tables
 plt.figure(2)
-plt.plot(temp, aw_Baldi88_50kPa*1e4, label="Baldi et al., 1988 (p=50 kPa)")
-plt.plot(temp, aw_Baldi88_200kPa*1e4, label="Baldi et al., 1988 (p=200 kPa)")
-plt.plot(temp, aw_Baldi88_400kPa*1e4, label="Baldi et al., 1988 (p=400 kPa)")
-plt.plot(temp, aw_Baldi88_1000kPa*1e4, label="Baldi et al., 1988 (p=1000 kPa)")
-plt.plot(temp, aw_IAPWS95_p[0]*1e4, 'x', label="IAPWS-95 (p=50 kPa)")
-plt.plot(temp, aw_IAPWS95_p[1]*1e4, '+', label="IAPWS-95 (p=200 kPa)")
-plt.plot(temp, aw_IAPWS95_p[2]*1e4, 's', label="IAPWS-95 (p=400 kPa)")
-plt.plot(temp, aw_IAPWS95_p[3]*1e4, 'v', label="IAPWS-95 (p=10000 kPa)")
-plt.plot(temp, aw_IAPWS95_fit*1e4, 'k-', label="IAPWS-95 (3rd degree poly fit)")
+plt.plot(temp, bw_Baldi88_50kPa*1e4, label="Baldi et al., 1988 (p=50 kPa)")
+plt.plot(temp, bw_Baldi88_200kPa*1e4, label="Baldi et al., 1988 (p=200 kPa)")
+plt.plot(temp, bw_Baldi88_400kPa*1e4, label="Baldi et al., 1988 (p=400 kPa)")
+plt.plot(temp, bw_Baldi88_1000kPa*1e4, label="Baldi et al., 1988 (p=1000 kPa)")
+plt.plot(temp, bw_IAPWS95_p[0]*1e4, 'x', label="IAPWS-95 (p=50 kPa)")
+plt.plot(temp, bw_IAPWS95_p[1]*1e4, '+', label="IAPWS-95 (p=200 kPa)")
+plt.plot(temp, bw_IAPWS95_p[2]*1e4, 's', label="IAPWS-95 (p=400 kPa)")
+plt.plot(temp, bw_IAPWS95_p[3]*1e4, 'v', label="IAPWS-95 (p=10000 kPa)")
+plt.plot(temp, bw_IAPWS95_fit*1e4, 'k-', label="IAPWS-95 (3rd degree poly fit)")
 plt.title("Figure 2b of Coulibaly and Rotta Loria 2022")
 plt.xlabel('Temperature [degC]')
 plt.ylabel('Volumetric thermal expansion of water [$10^{-4}$ 1/degC]')
 plt.legend()
 
 np.savetxt("tab_comparison_pressure_IAPWS95_Baldi88.csv",
-           np.concatenate((temp[:,np.newaxis], aw_Baldi88_50kPa[:,np.newaxis],
-                           aw_Baldi88_200kPa[:,np.newaxis],
-                           aw_Baldi88_400kPa[:,np.newaxis],
-                           aw_Baldi88_1000kPa[:,np.newaxis],
-                           aw_IAPWS95_p[0][:,np.newaxis],
-                           aw_IAPWS95_p[1][:,np.newaxis],
-                           aw_IAPWS95_p[2][:,np.newaxis],
-                           aw_IAPWS95_p[3][:,np.newaxis],
-                           aw_IAPWS95_fit[:,np.newaxis]),
+           np.concatenate((temp[:,np.newaxis], bw_Baldi88_50kPa[:,np.newaxis],
+                           bw_Baldi88_200kPa[:,np.newaxis],
+                           bw_Baldi88_400kPa[:,np.newaxis],
+                           bw_Baldi88_1000kPa[:,np.newaxis],
+                           bw_IAPWS95_p[0][:,np.newaxis],
+                           bw_IAPWS95_p[1][:,np.newaxis],
+                           bw_IAPWS95_p[2][:,np.newaxis],
+                           bw_IAPWS95_p[3][:,np.newaxis],
+                           bw_IAPWS95_fit[:,np.newaxis]),
                           axis=1),
-           header=("temperature_degC,aw_Baldi88_50kPa_1perdegC,"+
-                   "aw_Baldi88_200kPa_1perdegC,aw_Baldi88_400kPa_1perdegC,"+
-                   "aw_Baldi88_1000kPa_1perdegC,aw_IAPWS95_50kPa_1perdegC,"+
-                   "aw_IAPWS95_200kPa_1perdegC,aw_IAPWS95_400kPa_1perdegC,"+
-                   "aw_IAPWS95_1000kPa_1perdegC,"+
-                   "aw_IAPWS95_fit_d3_t20-80_p50_1000_1perdegC"),
+           header=("temperature_degC,bw_Baldi88_50kPa_1perdegC,"+
+                   "bw_Baldi88_200kPa_1perdegC,bw_Baldi88_400kPa_1perdegC,"+
+                   "bw_Baldi88_1000kPa_1perdegC,bw_IAPWS95_50kPa_1perdegC,"+
+                   "bw_IAPWS95_200kPa_1perdegC,bw_IAPWS95_400kPa_1perdegC,"+
+                   "bw_IAPWS95_1000kPa_1perdegC,"+
+                   "bw_IAPWS95_fit_d3_t20-80_p50_1000_1perdegC"),
            delimiter=',')
